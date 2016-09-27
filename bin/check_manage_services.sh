b9084f02f4dbfa8e9ac217556b4ea9503e7e494c
zhouyq <zhouyq@goodrain.com>
2016-09-27 11:39:08 +0800

    add manage node service check script

---
+++ bin/check_manage_services.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+#=====================
+# Info    : Cloudbang manage node service check script
+# Author  : zhouyq@goodrain.com
+# Version : 1.0.0 
+# Histroy : 
+#=====================
+
+function PrintInfo {
+  infotype=$1
+  info=$2
+
+  if [ "$infotype" == "info" ];then
+    echo -e "\033[32minfo\033[0m: $2"
+  else
+    echo -e "\033[31merror:\033[0m $2"
+  fi
+
+}
+
+
+BaseServiceList="beanstalkd etcd mysqld docker"
+
+K8SList="kube-apiserver kube-controller-manager kube-scheduler"
+
+SystemList="nginx supervisor"
+
+
+clear
+
+# BaseServiceList
+echo -e "\033[42;37mCheck Base Services...\033[0m"
+for s in $BaseServiceList
+do
+    isok=`pgrep -f $s`
+    if [ "$isok" == "" ];then
+      PrintInfo error "$s is down,start service..."
+      service $s start && PrintInfo info "$s started."
+    else
+      PrintInfo info "$s is OK"
+    fi
+done
+
+# K8SList
+echo -e "\n\033[42;37mCheck Kubernetes services...\033[0m"
+for s in $K8SList
+do
+    isok=`pgrep -f $s`
+    if [ "$isok" == "" ];then
+      PrintInfo error "$s is down,start service..."
+      start $s && PrintInfo info "$s started."
+    else
+      PrintInfo info "$s is OK"
+    fi
+done
+
+
+# SystemList
+echo -e "\n\033[42;37mCheck Platform services...\033[0m"
+for s in $SystemList
+do
+    isok=`pgrep -f $s`
+    if [ "$isok" == "" ];then
+      PrintInfo error "$s is down,start service..."
+      service $s start && PrintInfo info "$s started."
+    else
+      PrintInfo info "$s is OK"
+    fi
+done
+
+
+# Docker containers
+echo -e "\n\033[42;37mCheck container services...\033[0m"
+dc-compose up -d
